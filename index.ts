3dc09677c8f785a89d9b2d0ef348009c54172974
Spencer Browning <81187390+spencerbrowning@users.noreply.github.com>
2025-09-01 14:58:40 -0600

    added mongoose.js

--- index.ts
+++ index.ts
@@ -1,8 +1,19 @@
 import express from 'express';
+import mongoose from 'mongoose';
+import bodyParser from 'body-parser';
+
 import routes from './src/routes/crmRoutes.ts';
 
 const app = express();
 const PORT = process.env.PORT || 3000;
+
+// mongoose connection
+mongoose.Promise = global.Promise;
+mongoose.connect('mongodb://localhost/CRMdb', {});
+
+// bodyParser setup
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
 
 routes(app);
 

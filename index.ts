076deda889ffd49eb8184c69fbbaee82541d3fd4
Spencer Browning <81187390+spencerbrowning@users.noreply.github.com>
2025-09-01 13:45:32 -0600

    added REST routes

--- index.ts
+++ index.ts
@@ -1,7 +1,10 @@
 import express from 'express';
+import routes from './src/routes/crmRoutes.ts';
 
 const app = express();
 const PORT = process.env.PORT || 3000;
+
+routes(app);
 
 app.get('/', (req, res) => {
   res.send(`Node and express server is running on port ${PORT}`);
